63a931f3144e0ca3245662ca5e0558f1b24e8823
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-01-19 13:52:53 +0100

    :sparkles: display connection status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConnectionService } from './shared/services/connection.service';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+    public isOnline$ = this.connectionService.isOnline$;
+
+    constructor(private connectionService: ConnectionService) {
+    }
+
 }
