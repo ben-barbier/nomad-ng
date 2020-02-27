ddb9878fd98cf8573db3554d511438a97cf890bc
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-02-27 15:25:41 +0100

    :sparkles: add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
-
 const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
